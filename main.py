039bc75d02102432539ed3bbecdeb1ef9a060c12
PyJr95 <93840587+PyJr95@users.noreply.github.com>
2021-11-09 00:14:17 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
-print("Stefan")
-print("Status zero")
+from fastapi import FastAPI
+
+app = FastAPI()
+
+# request Get methid url: "/"
+
+
+
+@app.get("/")
+def root():
+
+    return {"message": "Welcome to my api 1st instance"}
+
+@app.get("/posts")
+def get_post():
+    return {"data": "This is your post"}
+
+
+@app.get("/random")
+def get_data():
+    return {"post": "Another post"}
+
